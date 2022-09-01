0bfaefe5c7f002f2f3fc6e384433b21c1e4645e2
David Rodriguez Miranda <wolfs_pride@outlook.es>
2022-09-01 21:12:10 +0200

    Filter podcasts

--- src/views/MainPage.tsx
+++ src/views/MainPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Entry, ITunesResponse } from "../models/ITunesResponse";
 import { PodcastCard } from "../components/PodcastCard";
 import { selectEntries, setEntries } from "../store/slices/podcastSlice";
@@ -10,6 +10,21 @@ import { getHoursDifference } from "../util/getHoursDifference";
 export const MainPage: React.FC = () => {
   const entries = useAppSelector(selectEntries);
   const dispatch = useAppDispatch();
+  const [filterValue, setFilterValue] = useState<string>("");
+  const filteredEntries = useMemo<Entry[]>(() => {
+    if (filterValue !== "") {
+      return entries.filter(
+        (entry) =>
+          entry["im:name"].label
+            .toLowerCase()
+            .includes(filterValue.toLowerCase()) ||
+          entry["im:artist"].label
+            .toLowerCase()
+            .includes(filterValue.toLowerCase())
+      );
+    }
+    return entries;
+  }, [filterValue, entries]);
 
   useEffect(() => {
     initialize();
@@ -22,6 +37,10 @@ export const MainPage: React.FC = () => {
       : null;
 
     if (localEntries && localStorage.getItem("lastUpdated")) {
+      /*
+       * Calculate time difference in hours when last updated the local storage entries.
+       * If more than 24 hours passed, we reload the entries.
+       */
       getHoursDifference(
         new Date(localStorage.getItem("lastUpdated")!),
         new Date()
@@ -44,8 +63,18 @@ export const MainPage: React.FC = () => {
   return (
     <div>
       <div className="mainContainer">
+        <div className="flex justify-end items-center gap-4">
+          <span className="chipCounter">{filteredEntries.length}</span>
+          <input
+            className="w-96"
+            type="text"
+            placeholder="Filter podcasts..."
+            value={filterValue}
+            onChange={(e) => setFilterValue(e.target.value)}
+          />
+        </div>
         <div className="grid grid-cols-4 gap-x-4 gap-y-32">
-          {entries.map((entry: Entry) => (
+          {filteredEntries.map((entry: Entry) => (
             <PodcastCard
               title={entry["im:name"].label}
               author={entry["im:artist"].label}
