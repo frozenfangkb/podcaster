c4e07c37f0399108f66d85c8d8540fcbf2cbea93
David Rodriguez Miranda <wolfs_pride@outlook.es>
2022-09-02 10:31:48 +0200

    add podcast playing

--- src/components/PodcastEpisodeCard/index.tsx
+++ src/components/PodcastEpisodeCard/index.tsx
@@ -14,7 +14,7 @@ export const PodcastEpisodeCard: React.FC<Props> = (props: Props) => {
 
   useEffect(() => {
     const ep = list.find((x) => x.trackId.toString() === params.episodeId!);
-    console.log(ep?.description);
+    console.log(ep);
 
     setEpisode(ep ?? ({} as PodcastEpisode));
   }, [list]);
@@ -23,10 +23,12 @@ export const PodcastEpisodeCard: React.FC<Props> = (props: Props) => {
     <div className="card flex flex-col gap-4 col-span-7 h-min">
       <h2>{episode?.trackName}</h2>
       <p
+        className="italic"
         dangerouslySetInnerHTML={{
           __html: sanitizeHtml(episode?.description ?? "No description"),
         }}
       ></p>
+      <audio className="w-full mt-8" src={episode?.episodeUrl} controls />
     </div>
   );
 };
